d62cfedb141e3ed96683fbc951b4f809b59a6d87
Olga <olsha.raskosha@gmail.com>
2023-11-10 19:33:52 +0300

    Initial commit

---
+++ src/domain/Software.java
@@ -0,0 +1,10 @@
+package domain;
+import java.util.List;
+public class Software {
+    List<Staff> staff;
+    @Override
+    public String toString() {
+        return "Software [employees=" + staff + "]";
+    }
+
+}
